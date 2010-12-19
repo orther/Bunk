debcff249712d66e82edab0f04fd4b887b7254ff
Brandon Orther <brandon.orther@gmail.com>
2010-12-18 16:20:08 -0800

    Started simple http_test web service REST tests/examples with database access.

--- actions/http_test.py
+++ actions/http_test.py
@@ -2,11 +2,38 @@ from bunk.action import BunkAction
 
 class HttpTestAction (BunkAction):
 
+    # ------------------------------------------------------------------------------------------------------------------
+    # SQL
+    # ------------------------------------------------------------------------------------------------------------------
+
+    sql_get_http_test_record = """
+    SELECT
+        id, route_id, request_ip, created_at
+    FROM
+        http_test
+    WHERE
+        id = %s
+    LIMIT 1
+    """
+
+    # ------------------------------------------------------------------------------------------------------------------
+
+    sql_insert_http_test_record = """
+    INSERT INTO http_test
+        (id, route_id, request_ip, created_at)
+    VALUES
+        (%(id)i, %(route_id)s, %(request_ip)s, NOW())
+    """
+
+    # ------------------------------------------------------------------------------------------------------------------
+    # METHODS
+    # ------------------------------------------------------------------------------------------------------------------
+
     def __init__ (self, route_id, **kwargs):
         """
         Create a new ExampleArgsAction instance.
 
-        @param sentence (str) The sentence to repeat to the client.
+        @param route_id (str) Route identifier set as an argument in the route.
         """
 
         BunkAction.__init__(self, **kwargs)
@@ -27,9 +54,8 @@ class HttpTestAction (BunkAction):
         dbconn = db.get_connection()
         dbcurs = dbconn.cursor()
 
-        # grab records
-        dbcurs.execute("SELECT * FROM test_tables LIMIT 1")
-
+        # pull record
+        dbcurs.execute(HttpTestAction.sql_get_http_test_record % client.params["id"])
         record = db.fetch_one(dbcurs)
 
         # close db connection
@@ -38,7 +64,7 @@ class HttpTestAction (BunkAction):
 
         client.compose_headers()
         client.write("GET Parameters: " + str(client.params) + "\n")
-        client.write("DB Result: " + str(record))
+        #client.write("DB Result: " + str(record))
         client.flush()
 
     # ------------------------------------------------------------------------------------------------------------------
@@ -59,6 +85,25 @@ class HttpTestAction (BunkAction):
         Handle a PUT request
         """
 
+        # initialize and connect to database
+        db     = self.init_db()
+        dbconn = db.get_connection()
+        dbcurs = dbconn.cursor()
+
+        # pull record
+        sql_params = {"id":         clients.params["id"],
+                      "route_id":   self._route_id,
+                      "request_ip": "Fake For Now"}
+
+        sql = HttpTestAction.sql_insert_http_test_record % client.params["id"]
+
+        dbcurs.execute(sql)
+
+        # close db connection
+        dbcurs.close()
+        dbconn.close()
+
         client.compose_headers()
-        client.write("PUT Parameters: " + str(client.params))
+        client.write("PUT Parameters: " + str(client.params) + "\n")
+        #client.write("DB Result: " + str(record))
         client.flush()
--- routing.py
+++ routing.py
@@ -3,5 +3,5 @@ from actions import http_test
 
 # routes
 routes = {
-    "/http_test": ("(test_id:\w+)", http_test.HttpTestAction, {"route_id": "http_test_1"})
+    "/http_test": ("(id:\w+)", http_test.HttpTestAction, {"route_id": "http_test_1"})
 }
