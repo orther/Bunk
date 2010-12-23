340a425c3dea0f3720c5607c9f39e311bd4d2f3b
Brandon Orther <brandon.orther@gmail.com>
2010-12-23 15:57:37 -0800

    added a http_test.sql in the action dir so I can setup the db simply when testing. I plan on removing this file.

---
+++ actions/http_test.sql
@@ -0,0 +1,21 @@
+-- --
+-- Bunk http_test SQL
+-- --
+
+-- create bunk_db database
+create database bunk_db;
+
+-- create bunk user and grant privileges to bunk_db
+grant usage on *.* to bunk@localhost identified by 'RESTing';
+grant all privileges on bunk_db.* to bunk@localhost;
+flush privileges;
+
+-- create http_test table
+use bunk_db;
+CREATE TABLE IF NOT EXISTS `http_test` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `route_id` varchar(255) NOT NULL,
+  `request_ip` varchar(30) NOT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
