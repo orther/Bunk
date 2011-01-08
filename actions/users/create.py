3a982260e2b25a0614dfe36626cb5d0edfc314a6
Brandon Orther <brandon.orther@gmail.com>
2011-01-07 19:25:15 -0800

    Finished the user create action.

--- actions/users/create.py
+++ actions/users/create.py
@@ -1,9 +1,24 @@
 from datetime import datetime
 
+from elements.core.exception import DatabaseModelException
+from elements.http           import response_code
+
 from bunk.action                              import BunkAction
 from bunk.response_formatters.json_formatter  import JsonFormatter
 
 from models.user import UserDBModel
+
+from settings import app_url_base
+
+# ----------------------------------------------------------------------------------------------------------------------
+# RESPONSE ERROR CODES
+# ----------------------------------------------------------------------------------------------------------------------
+
+RESP_ERR_CODE_USER_DETAILS_INVALID    = 1
+RESP_ERR_CODE_USER_CREATION_FAILED    = 2
+RESP_ERR_CODE_USERNAME_ALREADY_IN_USE = 3
+
+# ----------------------------------------------------------------------------------------------------------------------
 
 class UsersCreateAction (BunkAction):
 
@@ -22,21 +37,63 @@ class UsersCreateAction (BunkAction):
 
     def bunk_post (self):
         """
-        Create a user.
+        Create a user and return user_id.
+
+        @request_param username (str)
+        @request_param password (str)
+        @request_param email    (str)
+
+        @response user_id (int)
         """
 
         user = UserDBModel()
 
-        user.password = self._client.params.get("password")
-        user.username = self._client.params.get("username")
         user.email    = self._client.params.get("email")
+        user.password = str(self._client.params.get("password"))
+        user.username = self._client.params.get("username")
 
-        print user.validate()
-        print "Errors:", user.errors()
+        if user.validate():
+            try:
+                # get db connection and cursor
+                db     = self.get_db()
+                dbconn = db.get_connection()
+                dbcurs = dbconn.cursor()
 
-        # save the user
-        #user.save()
+                # check if user exists
+                existing_user = UserDBModel.filter([["username", "=", user.username]])()
 
-        #print user.values()
+                if existing_user:
+                    # user exists
+                    err_code   = RESP_ERR_CODE_USERNAME_ALREADY_IN_USE
+                    err_msg    = "Username already in use."
+                    field_errs = [('username', 'Username already in use. Please select another one.')]
 
-        #self.respond({"user_id": 1})
+                    return self.respond_error(err_code, err_msg, response_code.HTTP_409, field_errors=field_errs)
+
+                # create user
+                user.save(connection=dbconn)
+
+                # retrieve newely created user_id
+                dbcurs.execute("SELECT CURRVAL('user_user_id_seq') AS value")
+
+                user.user_id = db.fetch_all(dbcurs)[0]["value"]
+
+            except Exception, e:
+                # failed to create user for unknown reason
+                err_code = RESP_ERR_CODE_USER_CREATION_FAILED
+                err_msg  = "User failed to create for unknown reason."
+
+                return self.respond_error(err_code, err_msg, response_code.HTTP_500)
+
+        else:
+            # validation errors
+            err_code   = RESP_ERR_CODE_USER_DETAILS_INVALID
+            err_msg    = "One of more user detail(s) are invalid and must be fixed."
+            field_errs = user.errors().items()
+
+            return self.respond_error(err_code, err_msg, response_code.HTTP_400, field_errors=field_errs)
+
+        # user successfully created
+        self._client.out_headers["Location"] = "%s/users/%s.json" % (app_url_base, user.user_id)
+
+        return self.respond({"user_id": user.user_id}, response_code.HTTP_201)
